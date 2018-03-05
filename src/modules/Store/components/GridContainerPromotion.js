e87550424c13160dfcecee8f0f3eb9c335b4b217
DESKTOP-4UNROQ6\Andre.Araujo <andre.araujo@gestaosuper.com.br>
2018-03-05 14:11:30 -0300

    store card promotion

--- src/modules/Store/components/GridContainerPromotion.js
+++ src/modules/Store/components/GridContainerPromotion.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import CardPromotion from './CardPromotion';
 
 export default class GridContainerPromotion extends Component {
   render() {
     return (
       <div
         style={{
+          marginTop: '15%',
           display: 'grid',
           gridTemplateColumns: '40% 40%',
           gridColumnGap: '10px',
           justifyContent: 'center',
         }}
       >
-        <CardPromotion img="http://tedium.imgix.net/2017/03/0322_purple.jpg" />
-        <CardPromotion img="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAA1BMVEX//wCKxvRFAAAASElEQVR4nO3BgQAAAADDoPlTX+AIVQEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADwDcaiAAFXD1ujAAAAAElFTkSuQmCC" />
+        {this.props.children}
       </div>
     );
   }
--- src/modules/Store/index.js
+++ src/modules/Store/index.js
@@ -5,6 +5,7 @@ import GridContainer from './components/GridContainer';
 import { actionStore } from './actions/action';
 import Container from '../../components/Container/Container';
 import ContainerPromotion from './components/GridContainerPromotion';
+import CardPromotion from './components/CardPromotion';
 import './cardStyleVertical.css';
 
 class Store extends Component {
@@ -16,7 +17,10 @@ class Store extends Component {
     const { listStore } = this.props.StoreReducer;
     return (
       <Container link="Home" to="/">
-        <ContainerPromotion />
+        <ContainerPromotion>
+          <CardPromotion img="http://tedium.imgix.net/2017/03/0322_purple.jpg" />
+          <CardPromotion img="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAA1BMVEX//wCKxvRFAAAASElEQVR4nO3BgQAAAADDoPlTX+AIVQEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADwDcaiAAFXD1ujAAAAAElFTkSuQmCC" />
+        </ContainerPromotion>
         <GridContainer columns={['18%', '18%', '18%', '18%']}>
           {listStore &&
             listStore.map(data => (
