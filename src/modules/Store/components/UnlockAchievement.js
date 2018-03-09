effe353417f40f4c8b889ba2f04e6ea3f4ae916e
DESKTOP-4UNROQ6\Andre.Araujo <andre.araujo@gestaosuper.com.br>
2018-03-09 08:22:55 -0300

    pull

--- src/modules/Store/components/UnlockAchievement.js
+++ src/modules/Store/components/UnlockAchievement.js
@@ -5,98 +5,103 @@ import faCartPlus from '@fortawesome/fontawesome-free-solid/faCartPlus';
 export default class UnlockAchievement extends Component {
   render() {
     return (
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          width: '300px',
-          height: '120px',
-          backgroundColor: '#EEE5DE',
-          borderRadius: '10px',
-          position: 'fixed',
-          top: '110px',
-          right: '2px',
-          zIndex: 2,
-          padding: '2px',
-        }}
-      >
-        <div
+      <div>
+        <button id="show">oi</button>
+        <dialog
           style={{
-            width: '100%',
             display: 'flex',
+            flexDirection: 'column',
+            width: '300px',
+            height: '120px',
+            backgroundColor: '#EEE5DE',
+            borderRadius: '10px',
+            position: 'fixed',
+            top: '110px',
+            right: '2px',
+            zIndex: 2,
+            padding: '2px',
           }}
+          ref={element => (this.toast = element)}
+          open
         >
           <div
             style={{
+              width: '100%',
               display: 'flex',
-              width: '28%',
+            }}
+          >
+            <div
+              style={{
+                display: 'flex',
+                width: '28%',
+                marginLeft: '2px',
+                marginTop: '10px',
+                borderTopLeftRadius: '10px',
+                borderBottomLeftRadius: '10px',
+              }}
+            >
+              <img
+                style={{
+                  maxWidth: '60px',
+                  height: 'auto',
+                  alignSelf: 'baseline',
+                }}
+                src="https://theliteracyexperiments.files.wordpress.com/2015/08/icon-leaderboard.png"
+              />
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+              }}
+            >
+              <strong
+                style={{
+                  width: '100%',
+                  fontSize: '11pt',
+                }}
+              >
+                Conquista Desbloqueada
+              </strong>
+              <span style={{ fontSize: '9pt', color: '#5f5e5e' }}>
+                Parabéns pela primeira compra em nossa loja.
+              </span>
+              <span
+                style={{
+                  fontSize: '14pt',
+                  fontWeight: 'bold',
+                  color: 'green',
+                }}
+              >
+                1.500 Pt
+              </span>
+            </div>
+          </div>
+          <div
+            style={{
+              width: '100%',
+              display: 'flex',
               marginLeft: '2px',
-              marginTop: '10px',
-              borderTopLeftRadius: '10px',
-              borderBottomLeftRadius: '10px',
+              marginTop: '18px',
+              marginBottom: '5px',
             }}
           >
-            <img
+            <button
               style={{
-                maxWidth: '60px',
-                height: 'auto',
-                alignSelf: 'baseline',
-              }}
-              src="https://theliteracyexperiments.files.wordpress.com/2015/08/icon-leaderboard.png"
-            />
-          </div>
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-            }}
-          >
-            <strong
-              style={{
-                width: '100%',
-                fontSize: '11pt',
-              }}
-            >
-              Conquista Desbloqueada
-            </strong>
-            <span style={{ fontSize: '9pt', color: '#5f5e5e' }}>
-              Parabéns pela primeira compra em nossa loja.
-            </span>
-            <span
-              style={{
+                color: '#fff',
+                border: 'none',
+                background: '#ee2c2c',
+                borderRadius: '10px',
+                height: '24px',
+                width: '60px',
+                outline: 'none',
                 fontSize: '14pt',
-                fontWeight: 'bold',
-                color: 'green',
               }}
             >
-              1.500 Pt
-            </span>
+              <FontAwesomeIcon icon={faCartPlus} />
+            </button>
           </div>
-        </div>
-        <div
-          style={{
-            width: '100%',
-            display: 'flex',
-            marginLeft: '2px',
-            marginTop: '18px',
-            marginBottom: '5px',
-          }}
-        >
-          <button
-            style={{
-              color: '#fff',
-              border: 'none',
-              background: '#ee2c2c',
-              borderRadius: '10px',
-              height: '24px',
-              width: '60px',
-              outline: 'none',
-              fontSize: '14pt',
-            }}
-          >
-            <FontAwesomeIcon icon={faCartPlus} />
-          </button>
-        </div>
+        </dialog>
       </div>
     );
   }
