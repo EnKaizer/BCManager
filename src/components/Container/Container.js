bf15660a92ddec3a1eb68754626f6c08ef7523bb
Henrique Jensen <henriquejensen@hotmail.com>
2018-03-11 21:08:03 -0300

    ferrando com o header

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -11,117 +11,99 @@ import { Link } from 'react-router-dom';
 import { Profile } from '../../../src/modules/Profile/components';
 
 class Container extends Component {
-    state = { headerZ: 0, minHeader: false };
-    componentDidMount = () => {
-        const elem = ReactDOM.findDOMNode(this.elementToScroll);
-        if (window.addEventListener) {
-            window.addEventListener('scroll', this.headerLoad);
-            ReactDOM.findDOMNode(this.elementToScroll2).addEventListener(
-                'scroll',
-                this.headerLoad,
-            );
-            ReactDOM.findDOMNode(this.elementToScroll3).addEventListener(
-                'scroll',
-                this.headerLoad,
-            );
-        } else {
-            ReactDOM.findDOMNode(this.elementToScroll1).attachEvent(
-                'on' + 'scroll',
-                this.headerLoad,
-            );
-            ReactDOM.findDOMNode(this.elementToScroll2).attachEvent(
-                'on' + 'scroll',
-                this.headerLoad,
-            );
-            ReactDOM.findDOMNode(this.elementToScroll3).attachEvent(
-                'on' + 'scroll',
-                this.headerLoad,
-            );
-        }
-    };
-
-    componentWillUnmount() {
-        const elem = ReactDOM.findDOMNode(this.elementToScroll);
-        //elem.removeEventListener('scroll', this.headerLoad, true);
+  state = { headerZ: 0, minHeader: false };
+  componentDidMount = () => {
+    const elem = ReactDOM.findDOMNode(this.elementToScroll);
+    if (window.addEventListener) {
+      window.addEventListener('scroll', this.headerLoad);
+      ReactDOM.findDOMNode(this.elementToScroll2).addEventListener(
+        'scroll',
+        this.headerLoad,
+      );
+      ReactDOM.findDOMNode(this.elementToScroll3).addEventListener(
+        'scroll',
+        this.headerLoad,
+      );
+    } else {
+      ReactDOM.findDOMNode(this.elementToScroll1).attachEvent(
+        'on' + 'scroll',
+        this.headerLoad,
+      );
+      ReactDOM.findDOMNode(this.elementToScroll2).attachEvent(
+        'on' + 'scroll',
+        this.headerLoad,
+      );
+      ReactDOM.findDOMNode(this.elementToScroll3).attachEvent(
+        'on' + 'scroll',
+        this.headerLoad,
+      );
     }
+  };
 
-    shouldComponentUpdate = (nxtProps, nxtState) => {
-        if (nxtState.minHeader == this.state.minHeader)
-            return false;
-        else
-            return true;
-    };
+  componentWillUnmount() {
+    const elem = ReactDOM.findDOMNode(this.elementToScroll);
+    //elem.removeEventListener('scroll', this.headerLoad, true);
+  }
 
-    headerLoad = () => {
-        if (window.scrollY >= 610) {
-            this.setState({ minHeader: true }, () => {
-                $('#header').css({
-                    'z-index': 2,
-                    'line-height': '50px',
-                    height: '100px',
-                    position: 'fixed',
-                });
-            });
-        }
-        const windowHeight = $(window).height(),
-            heightDocument = windowHeight + $('.content').height() - 20;
+  shouldComponentUpdate = (nxtProps, nxtState) => {
+    if (nxtState.minHeader == this.state.minHeader) return false;
+    else return true;
+  };
 
-        // Definindo o tamanho dos elementos header e conteúdo
-        if (window.scrollY < 610) {
-            this.setState({ minHeader: false }, () => {
-                $('header').css({
-                    'z-index': 0,
-                    height: windowHeight + 'px',
-                    'line-height': windowHeight + 'px',
-                });
+  headerLoad = () => {
+    if (window.scrollY >= 610) {
+      this.setState({ minHeader: true }, () => {
+        $('#header').css({
+          'z-index': 2,
+          'line-height': '50px',
+          height: '100px',
+          position: 'fixed',
+        });
+      });
+    }
+    const windowHeight = $(window).height(),
+      heightDocument = windowHeight + $('.content').height() - 20;
 
-                $('.wrapper-parallax').css({
-                    'margin-top': windowHeight + 'px',
-                });
-            });
-        }
-    };
+    // Definindo o tamanho dos elementos header e conteúdo
+    if (window.scrollY < 610) {
+      this.setState({ minHeader: false }, () => {
+        $('header').css({
+          'z-index': 0,
+          height: windowHeight + 'px',
+          'line-height': windowHeight + 'px',
+        });
 
-    render() {
-        return (
-            <div
-                ref={element => (this.elementToScroll1 = element)}
-                className="App"
-            >
-                <div
+        $('.wrapper-parallax').css({
+          'margin-top': windowHeight + 'px',
+        });
+      });
+    }
+  };
 
-                    ref={element => (this.elementToScroll2 = element)}
-                    id="scroll-animate"
-                >
-                    <div
+  render() {
+    return (
+      <div ref={element => (this.elementToScroll1 = element)} className="App">
+        <div
+          ref={element => (this.elementToScroll2 = element)}
+          id="scroll-animate"
+        >
+          <div
+            ref={element => (this.elementToScroll3 = element)}
+            id="scroll-animate-main"
+          >
+            <Profile />
 
-                        ref={element => (this.elementToScroll3 = element)}
-                        id="scroll-animate-main"
-                    >
-                        <div className="wrapper-parallax">
-                            <header style={{ zIndex: 0 }} id="header">
-                                {this.state.minHeader ? (
-                                    <div style={{ paddingBottom: 10, color: 'white' }}>
-                                        <Link to={this.props.to}>{this.props.link}</Link>
-                                    </div>
-                                ) : (
-                                        <Profile />
-                                    )}
-                            </header>
-                            <div className="content">
-                                {this.props.spacer ? (
-                                    <div>
-                                        {this.props.children}
-                                    </div>
-                                ) : (
-                                        <div>{this.props.children}</div>
-                                    )}
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div className="content">
+              {this.props.spacer ? (
+                <div>{this.props.children}</div>
+              ) : (
+                <div>{this.props.children}</div>
+              )}
             </div>
-        );
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default DragDropContext(MouseBackEnd, HTML5Backend)(Container);
